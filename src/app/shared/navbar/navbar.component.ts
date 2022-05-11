9351b890e3bc8800363d99fd836726723701623c
pauttjordan <pauttjordan@gamil.com>
2022-05-11 14:13:33 -0500

    app deploy hosting firebase

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filter, Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
+export class NavbarComponent implements OnInit, OnDestroy {
+  nameUser: string = '';
+  userSubcription$: Subscription;
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.userSubcription$ = this.store
+      .select('user')
+      .pipe(filter(({ user }) => user !== null))
+      .subscribe(({ user }) => {
+        this.nameUser = user.name;
+      });
   }
-
+  
+  ngOnDestroy(): void {
+    this.userSubcription$?.unsubscribe();
+  }
 }
